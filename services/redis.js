b9c7e4dda386e0c26f4a204874b685d60030a2f0
cleriosouto <you@example.comclerio.souto@gmail.com>
2026-07-26 12:40:12 +0200

    Finalizar backend LovABLE MASTER com KV

--- services/redis.js
+++ services/redis.js
@@ -1,3 +1,30 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
 
-export { kv };
+
+let redis;
+
+
+try {
+
+    redis = new Redis({
+
+        url:
+            process.env.KV_REST_API_URL,
+
+        token:
+            process.env.KV_REST_API_TOKEN
+
+    });
+
+
+} catch (e) {
+
+    console.log(
+        "Redis config error:",
+        e.message
+    );
+
+}
+
+
+export const kv = redis;
